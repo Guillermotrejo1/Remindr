3b0d8d8cff2be27b4c86fe87a7405dfcc4d57de0
Guillermo Trejo <maynardtrejo@yahoo.com>
2026-05-05 21:48:29 -0500

    added the vercel url to the auth to redirect user after successful login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
+const isProd = typeof window !== "undefined" && window.location.hostname !== "localhost";
+
 export const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_9NbJioeJB",
   client_id: "7ktgavkau3bmmnlvm8ok6couh1",
-  redirect_uri: "http://localhost:3000",
+  redirect_uri: isProd ? "https://remindr-iota.vercel.app" : "http://localhost:3000",
   response_type: "code",
   scope: "phone openid email",
 };
